d1af485b90b6084802cd031ebcbfd78b0cb540b1
serdar <salciserdar@yahoo.com>
2020-12-27 02:11:24 -0500

    end of part 63

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -3,11 +3,7 @@ import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-const PrivateRoute = ({
-	component: Component,
-	auth: { isAuthenticated, loading },
-	...rest
-}) => (
+const PrivateRoute = ({ component: Component, auth: { isAuthenticated, loading }, ...rest }) => (
 	<Route
 		{...rest}
 		render={(props) =>
